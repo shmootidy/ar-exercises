148398fdae8fe268fb72f4acc746017521a0660c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 22:17:09 +0000

    Change callback to after_create. Works the same, except the password is not printed to the console upon insertion into the DB. I did not need to make any changes other than the name of the callback method used.

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,7 +4,8 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
 
-  before_create :set_password
+  # before_create :set_password
+  after_create :set_password
 
   private
     def set_password
