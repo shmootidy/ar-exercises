d75fa853b72c28206df26b6737630691d95c926d
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 21:06:45 +0000

    Display each error on a new line.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name and press enter:"
+@store_name = gets.chomp
+@new_store = Store.create(name: "#{@store_name}")
+@new_store.errors.full_messages.each do |message|
+  puts message
+end
+
+# https://stackoverflow.com/questions/11113394/how-do-i-capture-error-messages-from-activerecord-validations
