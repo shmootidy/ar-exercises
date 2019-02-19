19d64dba6a8cf4e96b573226113fe74c6015330f
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 22:14:39 +0000

    Add before_create callback method on Employee class to generate a password for new employees.

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -6,3 +6,8 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
+
+@store4 = Store.find(4)
+
+jack = @store4.employees.create(first_name: "Jack", last_name: "Beanstalkson", hourly_rate: 45)
+puts "#{jack.first_name}'s password is #{jack.password}. Keep it secret. Keep it safe."
--- lib/employee.rb
+++ lib/employee.rb
@@ -2,6 +2,13 @@ class Employee < ActiveRecord::Base
   belongs_to :store
 
   validates :first_name, :last_name, :store_id, presence: true
-  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200}
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+
+  before_create :set_password
+
+  private
+    def set_password
+      self.password = (0...8).map { (65 + rand(26)).chr }.join
+    end
 
 end
