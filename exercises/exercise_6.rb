afdaba208b031362c991c85df646078ed254805e
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 20:35:58 +0000

    Create new employees, within stores.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Blow", hourly_rate: 20)
+@store1.employees.create(first_name: "Carly R.", last_name: "Jepsen", hourly_rate: 22)
+@store2.employees.create(first_name: "Cher", last_name: "(just Cher)", hourly_rate: 600000)
+@store2.employees.create(first_name: "Mary", last_name: "Lamb", hourly_rate: 10)
+@store2.employees.create(first_name: "Koala", last_name: "Koalaface", hourly_rate: 20)
